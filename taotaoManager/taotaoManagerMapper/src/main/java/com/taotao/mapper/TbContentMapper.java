dfbffd3ff04587faf2e4296480f3a565fa1b4a44
junior <sunybyjava@gmail.com>
2017-05-29 15:20:36 +0800

    使用mybatis逆向工具生成完mapper文件以及实体类

---
+++ taotaoManager/taotaoManagerMapper/src/main/java/com/taotao/mapper/TbContentMapper.java
@@ -0,0 +1,37 @@
+package com.taotao.mapper;
+
+import java.util.List;
+
+import com.taotao.pojo.TbContent;
+import com.taotao.pojo.TbContentExample;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbContentMapper {
+    int countByExample(TbContentExample example);
+
+    int deleteByExample(TbContentExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbContent record);
+
+    int insertSelective(TbContent record);
+
+    List<TbContent> selectByExampleWithBLOBs(TbContentExample example);
+
+    List<TbContent> selectByExample(TbContentExample example);
+
+    TbContent selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbContent record, @Param("example") TbContentExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") TbContent record, @Param("example") TbContentExample example);
+
+    int updateByExample(@Param("record") TbContent record, @Param("example") TbContentExample example);
+
+    int updateByPrimaryKeySelective(TbContent record);
+
+    int updateByPrimaryKeyWithBLOBs(TbContent record);
+
+    int updateByPrimaryKey(TbContent record);
+}
---
+++ taotaoManager/taotaoManagerMapper/src/main/java/com/taotao/mapper/TbItemMapper.java
@@ -0,0 +1,32 @@
+package com.taotao.mapper;
+
+
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface TbItemMapper {
+    int countByExample(TbItemExample example);
+
+    int deleteByExample(TbItemExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbItem record);
+
+    int insertSelective(TbItem record);
+
+    List<TbItem> selectByExample(TbItemExample example);
+
+    TbItem selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbItem record, @Param("example") TbItemExample example);
+
+    int updateByExample(@Param("record") TbItem record, @Param("example") TbItemExample example);
+
+    int updateByPrimaryKeySelective(TbItem record);
+
+    int updateByPrimaryKey(TbItem record);
+}
