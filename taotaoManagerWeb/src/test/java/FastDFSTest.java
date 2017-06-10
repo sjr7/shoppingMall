7ee4675639a801f2401070b596c4aa9b52799825
sunjianrong <sunybyjava@gmail.com>
2017-06-10 22:18:38 +0800

    合并github上的项目

---
+++ taotaoManagerWeb/src/test/java/FastDFSTest.java
@@ -0,0 +1,24 @@
+/**
+ * Created by 孙建荣 on 17-6-10.下午6:01
+ */
+public class FastDFSTest {
+
+    public void testFastFds() {
+
+        //       1、加载配置文件，配置文件中的内容就是tracker服务的地址。
+
+        //       配置文件内容：tracker_server=192.168.25.133:22122
+
+        //       2、创建一个TrackerClient对象。直接new一个。
+
+        //       3、使用TrackerClient对象创建连接，获得一个TrackerServer对象。
+
+        //       4、创建一个StorageServer的引用，值为null
+
+        //       5、创建一个StorageClient对象，需要两个参数TrackerServer对象、StorageServer的引用
+
+        //       6、使用StorageClient对象上传图片。
+
+        //       7、返回数组。包含组名和图片的路径。
+    }
+}
