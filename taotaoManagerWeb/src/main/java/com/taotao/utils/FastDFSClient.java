9d4b998c0f7e40d10ef25332bbbb3c5accdb7a76
sunjianrong <sunybyjava@gmail.com>
2017-06-11 10:31:05 +0800

    测试完本机FastDfs的畅通性，完成图片上传工具类的编写

---
+++ taotaoManagerWeb/src/main/java/com/taotao/utils/FastDFSClient.java
@@ -0,0 +1,110 @@
+package com.taotao.utils;
+
+import org.csource.common.NameValuePair;
+import org.csource.fastdfs.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * FastDFS图片上传工具类
+ * Created by 孙建荣 on 17-6-11.上午9:59
+ */
+public class FastDFSClient {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private TrackerClient trackerClient = null;
+    private TrackerServer trackerServer = null;
+    private StorageServer storageServer = null;
+    private StorageClient1 storageClient = null;
+
+    public FastDFSClient(String conf) {
+        if (conf.contains("classpath:")) {
+            conf = conf.replace("classpath:", this.getClass().getResource("/").getPath());
+        }
+        try {
+            ClientGlobal.init(conf);
+        } catch (Exception e) {
+            logger.error("读取配置文件失败");
+            e.printStackTrace();
+        }
+        trackerClient = new TrackerClient();
+        try {
+            trackerServer = trackerClient.getConnection();
+        } catch (IOException e) {
+            logger.error("创建连接失败");
+            e.printStackTrace();
+        }
+        storageServer = null;
+        storageClient = new StorageClient1(trackerServer, storageServer);
+
+    }
+
+    /**
+     * 上传文件方法
+     *
+     * @param fileName 文件的全路径
+     * @param extName  文件的扩展名,不包括 .
+     * @param metas    文件扩展名信息
+     * @return
+     */
+    private String uploadFile(String fileName, String extName, NameValuePair[] metas) {
+        try {
+            return storageClient.upload_appender_file1(fileName, extName, metas);
+        } catch (Exception e) {
+            logger.error("文件上传发生了错误");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String uploadFIle(String fileName) {
+        return uploadFile(fileName, null, null);
+    }
+
+    public String uploadFile(String fileName, String extName) {
+        return uploadFile(fileName, extName, null);
+    }
+
+    /**
+     * 上传文件方法
+     *
+     * @param fileContent 文件的内容，为字节数组
+     * @param extName     文件的扩展名
+     * @param meta        文件的扩展信息
+     * @return
+     */
+    private String uploadFile(byte[] fileContent, String extName, NameValuePair[] meta) {
+        try {
+            return storageClient.upload_file1(fileContent, extName, meta);
+        } catch (Exception e) {
+            logger.error("发生了错误");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String uploadFile(byte[] fileContent) {
+        return uploadFile(fileContent, null, null);
+    }
+
+    public String uploadFile(byte[] fileContent, String extName) {
+        return uploadFile(fileContent, extName, null);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- taotaoManagerWeb/src/test/java/FastDFSTest.java
+++ taotaoManagerWeb/src/test/java/FastDFSTest.java
@@ -1,24 +1,45 @@
+import org.csource.fastdfs.*;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by 孙建荣 on 17-6-10.下午6:01
  */
 public class FastDFSTest {
 
-    public void testFastFds() {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public void testFastFds() throws Exception {
 
         //       1、加载配置文件，配置文件中的内容就是tracker服务的地址。
-
+        ClientGlobal.init("/home/sunjianrong/IdeaProjects/shoppingMall/taotaoManagerWeb/src/main/resources/resource/client.conf");
         //       配置文件内容：tracker_server=192.168.25.133:22122
-
         //       2、创建一个TrackerClient对象。直接new一个。
-
+        TrackerClient trackerClient = new TrackerClient();
         //       3、使用TrackerClient对象创建连接，获得一个TrackerServer对象。
-
+        TrackerServer trackerServer = trackerClient.getConnection();
         //       4、创建一个StorageServer的引用，值为null
-
+        StorageServer storageServer = null;
         //       5、创建一个StorageClient对象，需要两个参数TrackerServer对象、StorageServer的引用
-
+        StorageClient storageClient = new StorageClient(trackerServer, storageServer);
         //       6、使用StorageClient对象上传图片。
-
+        String[] result = storageClient.upload_file("/home/sunjianrong/IdeaProjects/shoppingMall/taotaoManagerWeb/src/main/webapp/WEB-INF/jsp/item-param-list.jsp", "jsp", null);
         //       7、返回数组。包含组名和图片的路径。
+        for (String s : result) {
+            System.out.println(s);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
