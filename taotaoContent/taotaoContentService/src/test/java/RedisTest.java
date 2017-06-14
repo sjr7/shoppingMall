9897f915230545e31f03d1bb6807994704b40f73
sunjianrong <sunybyjava@gmail.com>
2017-06-14 22:15:40 +0800

    完成对Redis单机版的测试

--- taotaoContent/taotaoContentService/src/test/java/RedisTest.java
+++ taotaoContent/taotaoContentService/src/test/java/RedisTest.java
@@ -1,11 +1,28 @@
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 /**
  * 测试redis
  * Created by 孙建荣 on 17-6-14.下午10:05
  */
 public class RedisTest {
+    @Test
+    public void testJedisPool() {
+        // 第一步创建一个jedisPool对象
+        JedisPool jedisPool = new JedisPool("127.0.0.1", 6379);
+        // 从池中取出jedis对象
+        Jedis jedis = jedisPool.getResource();
+        // 使用jedis操作redis服务器
+        jedis.set("jedis", "test");
+        String result = jedis.get("jedis");
+        System.out.println(result);
+        // 关闭连接
+        jedis.close();
+        // 关闭jedisPool对象
+        jedisPool.close();
+    }
+
     @Test
     public void testRedis() {
         // 第一步创建一个jredis对象，需要指定服务器的端口ip
