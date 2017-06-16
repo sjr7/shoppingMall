025d34e96272c071cbe47fbf3ec8c8c02cf6f5c7
sunjianrong <sunybyjava@gmail.com>
2017-06-16 22:39:56 +0800

    完成测试ActiveMQ Consumer

--- taotaoManager/taotaoManagerService/src/test/java/com/taotao/activemq/TestActivemq.java
+++ taotaoManager/taotaoManagerService/src/test/java/com/taotao/activemq/TestActivemq.java
@@ -4,6 +4,7 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.junit.Test;
 
 import javax.jms.*;
+import java.io.IOException;
 
 /**
  * Created by 孙建荣 on 17-6-16.下午9:54
@@ -12,6 +13,7 @@ public class TestActivemq {
 
     @Test
     public void testQueueProducer() throws JMSException {
+        // 测试Producer
         //1. 创建一个连接工厂对象ConnectionFactory对象,需要指定mq服务的ip跟端口
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
         //2. 使用ConnectionFactory创建一个连接Connection对象
@@ -36,9 +38,68 @@ public class TestActivemq {
         producer.close();
         session.close();
         connection.close();
-
     }
+
+    public void testConsumer() throws JMSException {
+        //   第一步：创建一个ConnectionFactory对象。
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+        //   第二步：从ConnectionFactory对象中获得一个Connection对象。
+        Connection connection = connectionFactory.createConnection();
+        //   第三步：开启连接。调用Connection对象的start方法。
+        connection.start();
+        //   第四步：使用Connection对象创建一个Session对象。
+        final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //   第五步：使用Session对象创建一个Destination对象。和发送端保持一致queue，并且队列的名称一致。
+        Queue queue = session.createQueue("test-queue");
+        //   第六步：使用Session对象创建一个Consumer对象。
+        MessageConsumer consumer = session.createConsumer(queue);
+        //   第七步：接收消息。
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                String text = null;
+                // 取消息的内容
+                try {
+                    text = textMessage.getText();
+                    // 打印消息
+                    System.out.println(text);
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        // 等待键盘输入
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //   第九步：关闭资源
+        consumer.close();
+        session.close();
+        connection.close();
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
