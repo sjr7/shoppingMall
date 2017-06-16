e821ffc39e8e57275c3974c3baa8a8f5f42fe152
sunjianrong <sunybyjava@gmail.com>
2017-06-16 22:51:03 +0800

    完成测试ActiveMQ Topic Consumer

--- taotaoManager/taotaoManagerService/src/test/java/com/taotao/activemq/TestActivemq.java
+++ taotaoManager/taotaoManagerService/src/test/java/com/taotao/activemq/TestActivemq.java
@@ -107,6 +107,47 @@ public class TestActivemq {
     }
 
 
+    @Test
+    public void testTopicConsumer() throws JMSException, IOException {
+        // 消费者：接收消息。
+        // 第一步：创建一个ConnectionFactory对象。
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+        // 第二步：从ConnectionFactory对象中获得一个Connection对象。
+        Connection connection = connectionFactory.createConnection();
+        // 第三步：开启连接。调用Connection对象的start方法。
+        connection.start();
+        // 第四步：使用Connection对象创建一个Session对象。
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 第五步：使用Session对象创建一个Destination对象。和发送端保持一致topic，并且话题的名称一致。
+        Topic topic = session.createTopic("test-topic");
+        // 第六步：使用Session对象创建一个Consumer对象。
+        MessageConsumer consumer = session.createConsumer(topic);
+        // 第七步：接收消息。
+        consumer.setMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(Message message) {
+                TextMessage textMessage = (TextMessage) message;
+                String text = null;
+                // 取消息的内容
+                try {
+                    text = textMessage.getText();
+                    // 第八步 打印出消息
+                    System.out.println(text);
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        System.out.println("topic的消费端");
+        // 第八步：打印消息。
+        System.in.read();
+        // 第九步：关闭资源
+        consumer.close();
+        session.close();
+        connection.close();
+    }
+
+
 }
 
 
