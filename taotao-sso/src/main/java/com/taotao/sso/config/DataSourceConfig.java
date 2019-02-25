2af91a78961e97b1d26ed1de37000af2e61668a4
sunjianrong <sunybyjava@gmail.com>
2019-02-25 16:16:23 +0800

    sso 模块重构

---
+++ taotao-sso/src/main/java/com/taotao/sso/config/DataSourceConfig.java
@@ -0,0 +1,14 @@
+package com.taotao.sso.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author sunjianrong
+ * @date 2019-02-25 下午 2:34
+ */
+@Configuration
+@EnableTransactionManagement
+public class DataSourceConfig{
+
+}
